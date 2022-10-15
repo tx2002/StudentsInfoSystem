650c05e5ec4df1fe30db1197f9022b5b0ceb3d5c
lxy-123-lxy <1139882458@qq.com>
2022-10-15 09:56:22 +0800

    普通类和老师类

--- src/main/java/com/example/studentsinfosystem/controller/CommonController.java
+++ src/main/java/com/example/studentsinfosystem/controller/CommonController.java
@@ -1,4 +1,34 @@
 package com.example.studentsinfosystem.controller;
 
+import com.example.studentsinfosystem.api.CommonResult;
+import com.example.studentsinfosystem.entity.Account;
+import com.example.studentsinfosystem.service.CommonService;
+import com.example.studentsinfosystem.utils.JwtToken;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+/**
+ * @author 梁鑫宇
+ * @date 2022/10/11
+ */
+@RestController
 public class CommonController {
+    @Autowired
+    JwtToken jwtToken = new JwtToken();
+
+    @Autowired
+    CommonService commonService;
+
+    @PostMapping("/login")
+    public CommonResult login(@RequestParam String username,@RequestParam String password){
+        String result = commonService.login(username,password);
+        if(result.equals("0"))
+            return CommonResult.failed("用户不存在");
+        if(result.equals("1"))
+            return  CommonResult.failed("密码错误");
+        else
+            return CommonResult.success(result);
+    }
+
+
 }
--- src/main/java/com/example/studentsinfosystem/service/Impl/TeacherServiceImpl.java
+++ src/main/java/com/example/studentsinfosystem/service/Impl/TeacherServiceImpl.java
@@ -1,4 +1,67 @@
 package com.example.studentsinfosystem.service.Impl;
 
-public class TeacherServiceImpl {
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.example.studentsinfosystem.entity.CourseInfo;
+import com.example.studentsinfosystem.entity.Score;
+import com.example.studentsinfosystem.entity.StudentInfo;
+import com.example.studentsinfosystem.mapper.CourseInfoMapper;
+import com.example.studentsinfosystem.mapper.ScoreMapper;
+import com.example.studentsinfosystem.mapper.StudentInfoMapper;
+import com.example.studentsinfosystem.mapper.TeacherInfoMapper;
+import com.example.studentsinfosystem.service.TeacherService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+
+@Service
+public class TeacherServiceImpl implements TeacherService {
+    @Autowired
+    TeacherInfoMapper teacherInfoMapper;
+    @Autowired
+    StudentInfoMapper studentInfoMapper;
+    @Autowired
+    ScoreMapper scoreMapper;
+    @Autowired
+    CourseInfoMapper courseInfoMapper;
+    @Override
+    public StudentInfo getStudentinfo(String studentId) {
+        QueryWrapper<StudentInfo> wrapper = new QueryWrapper<>();
+        wrapper.eq("student_id", studentId);
+        StudentInfo studentInfo = studentInfoMapper.selectOne(wrapper);
+        return studentInfo;
+    }
+
+
+    @Override
+    public StudentInfo changeStudentinfo(String token, StudentInfo studentInfo) {
+        return null;
+    }
+
+    @Override
+    public String inputStudent(String token, StudentInfo studentInfo) {
+        return null;
+    }
+
+        @Override
+        public String deleteStudent(String token, StudentInfo studentInfo) {
+            return null;
+        }
+
+    @Override
+    public List<StudentInfo> getAllStudentInfo(String token, String courseName) {
+        return null;
+    }
+
+    @Override
+    public List<Score> getAllStudentScore(String token, String studentId) {
+        return null;
+    }
+
+    @Override
+    public List<CourseInfo> getAllCourseInfo(String token, String username) {
+        return null;
+    }
+
 }
